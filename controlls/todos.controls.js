e3c27644d3c93160014d53f6f073429288be6590
Blnkfc <pavlinus11v@gmail.com>
2024-09-01 15:51:50 +0300

    Added controls folder and moved Todo requests functionality to them.

---
+++ controlls/todos.controls.js
@@ -0,0 +1,71 @@
+const Todo = require('../models/todos.model.js')
+
+const getTodos = async(req, res) => {
+    try{
+        const todos = await Todo.find({})
+        res.status(200).json(todos)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+const getTodoById = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const todo = await Todo.findById(id)
+        res.status(200).json(todo)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+const postTodo = async (req, res) => {
+    console.log("Received POST request:", req.body);
+    try{
+        const todo = await Todo.create(req.body)
+        res.status(200).json(todo)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+
+const updateTodo = async (req, res) => {
+    try{
+        const {id} = req.params
+        const todo = await Todo.findByIdAndUpdate(id, req.body)
+
+        if(!todo){
+            res.status(404).json({message: "Could not find requested todo"})
+        }
+
+        const updatedTodo = await Todo.findById(id)
+        res.status(200).json(updatedTodo)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+
+}
+
+const deleteTodo = async (req, res) => {
+    try{
+        const {id} = req.params
+        const todo = await Todo.findByIdAndDelete(id)
+        if(!todo){
+            res.status(404).json({message: "Could not find requested todo"})
+        }
+        res.status(200).json({message: 'Deleted the todo succesfully'})
+
+        
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = {
+    getTodos,
+    getTodoById,
+    postTodo,
+    updateTodo,
+    deleteTodo
+}
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -2,71 +2,22 @@ const express = require('express')
 const Todo = require('../models/todos.model.js')
 const router = express.Router()
 
+const {getTodos, getTodoById, postTodo, updateTodo, deleteTodo} = require('../controlls/todos.controls.js')
+
 //GET ALL TODOS
-router.get('/', async(req, res) => {
-    try{
-        const todos = await Todo.find({})
-        res.status(200).json(todos)
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-});
+router.get('/', getTodos);
 
 //GET TODO BY ID
-router.get('/:id', async (req, res) => {
-    try{
-        const {id} = req.params;
-        const todo = await Todo.findById(id)
-        res.status(200).json(todo)
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.get('/:id', getTodoById)
 
 //POST TODO
-router.post('/api/todos', async (req, res) => {
-    console.log("Received POST request:", req.body);
-    try{
-        const todo = await Todo.create(req.body)
-        res.status(200).json(todo)
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-});
+router.post('/', postTodo);
 
 //UPDATE TODO
-router.put('/api/todos/:id', async (req, res) => {
-    try{
-        const {id} = req.params
-        const todo = await Todo.findByIdAndUpdate(id, req.body)
-
-        if(!todo){
-            res.status(404).json({message: "Could not find requested todo"})
-        }
-
-        const updatedTodo = await Todo.findById(id)
-        res.status(200).json(updatedTodo)
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-
-})
+router.put('/:id', updateTodo)
 
 //DELETE TODO
-router.delete('/api/todos/:id', async (req, res) => {
-    try{
-        const {id} = req.params
-        const todo = await Todo.findByIdAndDelete(id)
-        if(!todo){
-            res.status(404).json({message: "Could not find requested todo"})
-        }
-        res.status(200).json({message: 'Deleted the todo succesfully'})
-
-        
-    }catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.delete('/:id', deleteTodo)
 
 
 
